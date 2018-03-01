6c083d5fbb9be435a4ce5be4b6a8ea1c3f190013
hendrik86 <hendrik.suherman86@gmail.com>
2018-03-01 13:09:29 +0700

    update RO Item

--- golden/__init__.py
+++ golden/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-__version__ = '1.10.10'
+__version__ = '1.10.11'
 
+# 1.10.11
+#   - Update Receive Order Item
 # 1.10.10
 #   - Update Receive Order
 # 1.10.9
