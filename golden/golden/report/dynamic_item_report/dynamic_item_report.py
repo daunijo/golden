c082cf07536a9aa03a7575873d49a907405270c6
hendrik86 <hendrik.suherman86@gmail.com>
2018-04-10 17:50:30 +0700

    update report

--- golden/golden/report/dynamic_item_report/dynamic_item_report.py
+++ golden/golden/report/dynamic_item_report/dynamic_item_report.py
@@ -76,10 +76,12 @@ def execute(filters=None):
 				# bin.append(cek)
 				cek = frappe.db.sql("""select `name` from `tabBin` where item_code = %s order by warehouse asc limit %s,1""", (cl.name, q))[0][0]
 				location = frappe.db.sql("""select warehouse from `tabBin` where `name` = %s""", cek)[0][0]
-				section = frappe.db.sql("""select parent from `tabWarehouse` where `name` = %s""", location)[0][0]
-				warehouse = frappe.db.sql("""select parent from `tabWarehouse` where `name` = %s""", section)[0][0]
+				# section = frappe.db.sql("""select parent from `tabWarehouse` where `name` = %s""", location)[0][0]
+				# warehouse = frappe.db.sql("""select parent from `tabWarehouse` where `name` = %s""", section)[0][0]
 				# actual_qty = frappe.db.sql("""select actual_qty from `tabBin` where `name` = %s""", cek)[0][0]
 				# bin_uom = frappe.db.sql("""select stock_uom from `tabBin` where `name` = %s""", cek)[0][0]
+				section = ""
+				warehouse = ""
 				actual_qty = ""
 				bin_uom = ""
 				binti = frappe.db.sql("""select `name` from `tabBin` where item_code = %s order by warehouse asc limit %s,1""", (cl.name, q))[0][0]
