6dbd6b0d177c004fcc3be8f09e33197f3bab2609
hendrik86 <hendrik.suherman86@gmail.com>
2018-05-02 17:20:02 +0700

    update sales target

--- golden/golden/doctype/sales_target/sales_target.py
+++ golden/golden/doctype/sales_target/sales_target.py
@@ -22,14 +22,14 @@ def get_sales_invoice(sales, start_date, end_date, target1, target2):
 	si_list = []
 	sales_invoice = frappe.db.sql("""select * from `tabSales Invoice` where docstatus = '1' and rss_sales_person = %s and posting_date between %s and %s""", (sales, start_date, end_date), as_dict=True)
 	for si in sales_invoice:
+		total_si = frappe.db.sql("""select sum(net_total) from `tabSales Invoice` where docstatus = '1' and rss_sales_person = %s and posting_date between %s and %s""", (sales, start_date, end_date))[0][0]
 		count_payment = frappe.db.sql("""select count(*) from `tabPayment Entry Reference` a inner join `tabPayment Entry` b on a.parent = b.`name` where b.docstatus = '1' and a.reference_name = %s order by posting_date desc limit 1""", si.name)[0][0]
-		contribution = (flt(si.net_total) / flt(target)) * 100
+		contribution = (flt(si.net_total) / flt(total_si)) * 100
 		if flt(count_payment) != 0:
-			pd = frappe.db.sql("""select posting_date, datediff(posting_date, %s) as diff_day from `tabPayment Entry Reference` a inner join `tabPayment Entry` b on a.parent = b.`name` where b.docstatus = '1' and a.reference_name = %s order by posting_date desc limit 1""", (si.posting_date, si.name), as_dict=1)
+			payment_date = frappe.db.sql("""select b.posting_date from `tabPayment Entry Reference` a inner join `tabPayment Entry` b on a.parent = b.`name` where b.docstatus = '1' and a.reference_name = %s order by posting_date desc limit 1""", si.name)[0][0]
+			diff_day = frappe.db.sql("""select datediff(b.posting_date, %s) from `tabPayment Entry Reference` a inner join `tabPayment Entry` b on a.parent = b.`name` where b.docstatus = '1' and a.reference_name = %s order by posting_date desc limit 1""", (si.posting_date, si.name))[0][0] or '0'
 			payment_amount = frappe.db.sql("""select sum(allocated_amount) from `tabPayment Entry Reference` a inner join `tabPayment Entry` b on a.parent = b.`name` where b.docstatus = '1' and a.reference_name = %s""", si.name)[0][0]
 			# diff_day = payment_date - si.posting_date
-			payment_date = pd.posting_date
-			diff_day = pd.diff_day
 		else:
 			payment_date = ""
 			payment_amount = 0
