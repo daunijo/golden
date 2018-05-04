5b655cba0603c97f3e1ba716aa3a62c1e8b449a3
hendrik86 <hendrik.suherman86@gmail.com>
2018-05-04 15:53:07 +0700

    update analysis

--- golden/golden/report/purchase_analysis/purchase_analysis.py
+++ golden/golden/report/purchase_analysis/purchase_analysis.py
@@ -16,9 +16,14 @@ def execute(filters=None):
 		items = frappe.db.sql("""select * from `tabItem` where item_group = %s and disabled = '0'""", cl.name, as_dict=1)
 		count_actual = 0
 		count_po = 0
+		stock_qty = 0
+		stock_value = 0
 		for it in items:
 			min_stock = it.minimum_stock
 			actual_qty = frappe.db.sql("""select sum(b.actual_qty) from `tabItem` i inner join `tabBin` b on i.item_code = b.item_code inner join `tabWarehouse` w on b.warehouse = w.`name` where w.gudang_bs = '0' and i.item_code = %s""", it.item_code)[0][0]
+			stock_qty = flt(stock_qty) + flt(actual_qty)
+			valuation = frappe.db.sql("""select sum(b.stock_value) from `tabItem` i inner join `tabBin` b on i.item_code = b.item_code inner join `tabWarehouse` w on b.warehouse = w.`name` where w.gudang_bs = '0' and i.item_code = %s""", it.item_code)[0][0]
+			stock_value = flt(stock_value) + flt(valuation)
 			if flt(actual_qty) >= flt(min_stock):
 				count_actual = count_actual + 1
 			else:
@@ -32,7 +37,7 @@ def execute(filters=None):
 			record = 0
 		else:
 			record = diff
-		data.append([cl.name, cl.minimum_item, count_actual, count_po, record])
+		data.append([cl.name, cl.minimum_item, count_actual, stock_qty, stock_value, count_po, record])
 
 	return columns, data
 
@@ -41,10 +46,12 @@ def get_columns():
 
 	columns = [
 		_("Item Group")+":Link/Item Group:150",
-		_("Minimum Item")+":Int:110",
-		_("Actual")+":Int:110",
-		_("Purchase Order")+":Int:110",
-		_("Record")+":Int:110",
+		_("Minimum Item")+":Int:100",
+		_("Actual")+":Int:100",
+		_("Stock Qty")+":Int:100",
+		_("Stock Value")+":Currency:120",
+		_("Purchase Order")+":Int:100",
+		_("Record")+":Int:100",
 	]
 
 	return columns
