59ae49b3b90eaa77cda2b4a9aeba239978dd3ef2
hendrik86 <hendrik.suherman86@gmail.com>
2018-05-18 03:21:12 -0700

    1.17.0

---
+++ golden/golden/doctype/template_warehouse/template_warehouse.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2018, RSS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Template Warehouse', {
+	refresh: function(frm) {
+
+	},
+	onload: function(frm) {
+		frm.set_query('warehouse', function(doc) {
+			return {
+				filters: {
+					"is_group": 1,
+					"type": "Warehouse"
+				}
+			}
+		})
+		frm.set_query("warehouse", "details", function(doc, cdt, cdn) {
+			var row = locals[cdt][cdn];
+			return {
+				filters: {
+					"is_group": 1,
+					"type": "Warehouse"
+				}
+			}
+		});
+	},
+});
---
+++ golden/golden/doctype/template_warehouse/template_warehouse.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, RSS and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from frappe.utils import nowdate, cstr, flt, now, getdate, add_months
+from frappe import msgprint, _
+from frappe.model.mapper import get_mapped_doc
+
+class TemplateWarehouse(Document):
+	pass
+
+@frappe.whitelist()
+def get_template_warehouse(tw):
+	si_list = []
+	sales_invoice = frappe.db.sql("""select * from `tabTemplate Warehouse Detail` where parent = %s order by idx asc""", (tw), as_dict=True)
+	for si in sales_invoice:
+		si_list.append(frappe._dict({
+	        'warehouse': si.warehouse
+	    }))
+	return si_list
