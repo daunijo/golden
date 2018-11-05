4997b8957cc035d0e1f729f588bebc94cf85ffdc
saint2228 <hrdabcgroups@gmail.com>
2018-11-05 15:09:37 +0700

    fix bugs hooks

--- golden/hooks.py
+++ golden/hooks.py
@@ -16,7 +16,7 @@ fixtures = ["Custom Script", "Custom Field"]
 # ------------------
 
 # include js, css files in header of desk.html
-app_include_css = "/assets/golden/css/golden.css"
+app_include_css = "/assets/golden/css/custom.css"
 #app_include_css = "/files/custom.css"
 # app_include_js = "/assets/golden/js/golden.js"
 
